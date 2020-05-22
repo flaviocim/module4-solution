56f128984137dcc2e419841a9055c942e3945352
Flavio <65506179+flaviocim@users.noreply.github.com>
2020-05-22 16:54:05 +0200

    Update SpeakGoodbye.js

--- SpeakGoodbye.js
+++ SpeakGoodbye.js
@@ -10,6 +10,3 @@
   window.byeSpeaker = byeSpeaker;
 })(window);
 
-// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
-// 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
